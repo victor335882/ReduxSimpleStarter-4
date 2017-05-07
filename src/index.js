93236ea0f32ca8402eedf024ba995680911d55c5
victor335882 <victor335882@livemail.tw>
2017-05-07 11:26:05 +0800

    Lecture 114~120 Route  concept
    http://localhost:8080/goodbye
    will show GoodBye! on screen

--- src/index.js
+++ src/index.js
@@ -2,17 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
+//BrowserRouter interactive with the history of browser and design what to do
+//Route is a react component we can use in anyother react components
+//it's give the configuation that if the url like this then show me the component
+import { BrowserRouter, Route} from 'react-router-dom';
 import ReduxPromise from 'redux-promise';
 
 import App from './components/app';
 import reducers from './reducers';
 
+class Hello extends React.Component {
+  render() {return <div> Hello! </div>}
+}
+class GoodBye extends React.Component {
+  render() {return <div>GoodBye</div>}
+}
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        {
+          //if user go to the Route of hello,
+          // we show the component of Hello
+        }
+        <Route path="/hello" component={Hello}/>
+        <Route path="/goodbye" component={GoodBye}/>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
