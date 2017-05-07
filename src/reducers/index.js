542faf9134e31acb8f90d69121d491035a20df7f
victor335882 <victor335882@livemail.tw>
2017-05-07 17:41:26 +0800

    Lecture 125.1 set the default reducers/index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import WeatherReducer from './reducer_weather';
+
 
 const rootReducer = combineReducers({
-  weather: WeatherReducer
+  state: (state = {}) => state
 });
 
 export default rootReducer;
