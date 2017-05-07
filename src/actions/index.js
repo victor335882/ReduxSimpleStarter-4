fc4232380bd0e3bcd6971cee7cade22b646e05db
victor335882 <victor335882@livemail.tw>
2017-05-07 23:17:48 +0800

    Lecture 125 react life cycle method, componentDidMount
    change unique victor335882

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,9 +5,9 @@ export const FETCH_POSTS = 'fecth_posts';
 //the example from http://reduxblog.herokuapp.com/
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api/'
 //self define key
-const API_KEY = `?key=PAPERCLIP1234`;
+const API_KEY = `?key=victor335882`;
 export function fetchPosts() {
-  const request = axios.get(`${POST_URL}/posts${API_KEY}`);
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
   return {
     type:FETCH_POSTS,
     payload:request
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -3,6 +3,21 @@ import {connect} from 'react-redux';
 import {fetchPosts} from '../actions';
 
 class PostsIndex extends Component {
+  //here we don't have event to trigger the action
+  //if this component is called,
+  //we want to reach out our api
+  //and immediately fetch our post, fetchPosts.
+
+  //so we use react life cycle methods,
+  //it's a function in react component class
+  //which will be automatically called by react
+
+  //The componentDidMount() method of child components
+  //is invoked before that of parent components.
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render () {
      return (
       <div>
