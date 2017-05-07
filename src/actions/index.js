f5510b6239cdcfd59ee6803c7324f67d144b0956
victor335882 <victor335882@livemail.tw>
2017-05-07 17:25:53 +0800

    Lecture 124 set our action creator
    when we think about make network request inside our action creator
    
    we need to install axios to make actual request
    
    and install react-promise to handle the asynchronous nature of the
    request itself

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const FETCH_POSTS = 'fecth_posts';
+
+//the example from http://reduxblog.herokuapp.com/
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api/'
+//self define key
+const API_KEY = `?key=PAPERCLIP1234`;
+export function fetchPosts() {
+  const request = axios.get(`${POST_URL}/posts${API_KEY}`);
+  return {
+    type:FETCH_POSTS,
+    payload:request
+  };
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route} from 'react-router-dom';
-import ReduxPromise from 'redux-promise';
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 
+//when we think about make network request inside our action creator
+//we need to install axios to make actual request
+//and install react-promise to handle the asynchronous nature of the request itself
+import ReduxPromise from 'redux-promise';
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
