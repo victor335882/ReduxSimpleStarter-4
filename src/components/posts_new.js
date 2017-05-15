e9341dc1ab99287bef2d74b0ea9a8707bc7e360d
victor335882 <victor335882@livemail.tw>
2017-05-15 22:12:33 +0800

    Lecture 134.2 use the attribute of field to create reused function of <Field/>

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,11 +2,14 @@ import React,{Component} from 'react';
 import {Field, reduxForm} from 'redux-form';
 
 class PostNew extends Component {
-
-  renderTitleField(field){
+  //each attribite in <Field/> tage will be
+  // pass in the variable field
+  renderField(field){
     return (
-      <div>
+      <div className="form-group">
+        <label>{field.label111}</label>
         <input
+          className="form-control"
           type="text"
           {...field.input}
         />
@@ -19,8 +22,19 @@ class PostNew extends Component {
     return(
       <form>
         <Field
+          label111="Title For Post"
           name="title"
-          component={this.renderTitleField}
+          component={this.renderField}
+        />
+        <Field
+          label111="Tags"
+          name="tags"
+          component={this.renderField}
+        />
+        <Field
+          label111="Post Content"
+          name="content"
+          component={this.renderField}
         />
       </form>
     );
