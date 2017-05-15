4e5932e9bcba555183dccce6a0ba493777ae062e
victor335882 <victor335882@livemail.tw>
2017-05-15 20:52:37 +0800

    Lecture 131 install redux form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,15 @@
 import { combineReducers } from 'redux';
+
+//if we want to use redux from we need import this
+//and add a key form equal to the value formReducer
+import { reducer as fromReducer } from 'redux-from';
 import PostsReducer from './reducer_posts';
 
+//every thing we import as redux from will be store
+//in form state
 const rootReducer = combineReducers({
-  posts:PostsReducer
+  posts:PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
