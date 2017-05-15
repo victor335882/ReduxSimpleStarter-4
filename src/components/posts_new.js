ba4959df3db31e6e0b344cd652ff1dc4082e188f
victor335882 <victor335882@livemail.tw>
2017-05-15 21:51:19 +0800

    Lecture 133 redux form field component
    //the field contain some event handler to connect
      //renderTitleField with our field
      //...field.input contains :
      //onChange={field.input.onChange}
      //onFocus={field.input.onFocus}
      //onBlur={field.input.onBlur}

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,15 +1,33 @@
 import React,{Component} from 'react';
 //reduxRorm is some kinds of connect between our Component
 //and the redux store with the key "from"
-import {Field, reduxFrom} from 'redux-from';
+import {Field, reduxForm} from 'redux-form';
 
 class PostNew extends Component {
+  //the field contain some event handler to connect
+  //renderTitleField with our field
+  //...field.input contains :
+  //onChange={field.input.onChange}
+  //onFocus={field.input.onFocus}
+  //onBlur={field.input.onBlur}
+  renderTitleField(field){
+    return (
+      <div>
+        <input
+          type="text"
+          {...field.input}
+        />
+      </div>
+    )
+
+  }
+
   render() {
     return(
       <form>
         <Field
           name="title"
-          component={}
+          component={this.renderTitleField}
         />
       </form>
     );
@@ -22,6 +40,6 @@ class PostNew extends Component {
 //the important thing is to make sure the string we assign
 //to the key form is unique
 //to prevent merge with other component's form
-export default reduxFrom({
+export default reduxForm({
   form: 'PostNewForm'
 })(PostNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 
 //if we want to use redux from we need import this
 //and add a key form equal to the value formReducer
-import { reducer as fromReducer } from 'redux-from';
+import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './reducer_posts';
 
 //every thing we import as redux from will be store
