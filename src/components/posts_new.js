92ca13be3b0955dce3cf1278dbc8a844d76aa88a
victor335882 <victor335882@livemail.tw>
2017-05-15 21:12:57 +0800

    Lecture 132  introduce how to using redux form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,12 +1,27 @@
 import React,{Component} from 'react';
+//reduxRorm is some kinds of connect between our Component
+//and the redux store with the key "from"
+import {Field, reduxFrom} from 'redux-from';
+
 class PostNew extends Component {
   render() {
     return(
-      <div>
-        PostsNew!
-      </div>
+      <form>
+        <Field
+          name="title"
+          component={}
+        />
+      </form>
     );
   }
 }
 
-export default PostNew;
+//connect PostNew to the redux reducer store
+//redusFrom will merge all states of each form
+//into a single piese form store
+//the important thing is to make sure the string we assign
+//to the key form is unique
+//to prevent merge with other component's form
+export default reduxFrom({
+  form: 'PostNewForm'
+})(PostNew);
