1e42ba93e1a6096bc16a09baaa2f35ab17c8d468
victor335882 <victor335882@livemail.tw>
2017-05-18 08:07:33 +0800

    Lecture 140 use <Link> to navigate between home page and post new page

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import {Field, reduxForm} from 'redux-form';
+import {Link} from 'react-router-dom';
 
 class PostNew extends Component {
 
@@ -49,6 +50,7 @@ class PostNew extends Component {
           component={this.renderField}
         />
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to = "/" className="btn btn-danger"> Cancel </Link>
       </form>
     );
   }
