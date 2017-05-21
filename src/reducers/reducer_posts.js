6446e5df2ec192f883da4cc87e0a64ffc48f7057
victor335882 <victor335882@livemail.tw>
2017-05-21 15:41:47 +0800

    Lecture 148  add a button to delete post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,13 @@
 import _ from 'lodash';
-import {FETCH_POSTS, FETCH_POST} from '../actions';
+import {FETCH_POSTS, FETCH_POST,DELETE_POST} from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case DELETE_POST:
+      //return a new state object without the id of action.payload
+      //which we just delete
+      //so we can delete the post from native server in the sametime
+      return _.omit(state, action.payload);
     case FETCH_POST:
       return {...state, [action.payload.data.id]:action.payload.data};
     case FETCH_POSTS:
